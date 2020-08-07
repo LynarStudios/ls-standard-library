fdbab1237760dfc5fb2c38b35366e596cc5c4da2
Patrick <webmaster@lynarstudios.com>
2020-08-07 17:02:21 +0200

    Added types
    - added Types header to introduce specific type aliases

---
+++ source/base/Types.hpp
@@ -0,0 +1,17 @@
+/*
+ * Author:          Patrick-Christopher Mattulat
+ * Company:         Lynar Studios
+ * E-Mail:          webmaster@lynarstudios.com
+ * Created:         2020-08-07
+ * Changed:         2020-08-07
+ *
+ * */
+
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+namespace ls_std {
+  using byte = char;
+}
+
+#endif
