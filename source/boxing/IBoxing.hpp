a553e938661890ce4d81ee7d9131e57d9c377f0e
Patrick <webmaster@lynarstudios.com>
2020-08-09 13:21:39 +0200

    Extended IBoxing interface
    - added "toString" method

--- source/boxing/IBoxing.hpp
+++ source/boxing/IBoxing.hpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2020-08-07
- * Changed:         2020-08-07
+ * Changed:         2020-08-09
  *
  * */
 
@@ -20,6 +20,7 @@ namespace ls_std {
       ~IBoxing() = default;
 
       virtual void parse(std::string parseText) = 0;
+      virtual std::string toString() = 0;
   };
 }
 
