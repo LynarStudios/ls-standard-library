6758a7b692a9414af84cf5d5390806a12d6d3605
Patrick <webmaster@lynarstudios.com>
2020-08-15 09:10:28 +0200

    Added TestHelper class
    - added TestHelper class for providing test location

---
+++ test/TestHelper.hpp
@@ -0,0 +1,26 @@
+/*
+ * Author:          Patrick-Christopher Mattulat
+ * Company:         Lynar Studios
+ * E-Mail:          webmaster@lynarstudios.com
+ * Created:         2020-08-15
+ * Changed:         2020-08-15
+ *
+ * */
+
+#ifndef TEST_HELPER_HPP
+#define TEST_HELPER_HPP
+
+#include <string>
+
+class TestHelper {
+  public:
+
+    TestHelper() = default;
+    ~TestHelper() = default;
+
+    static std::string getTestFolderLocation() {
+      return R"(C:\Users\drums\CLionProjects\lynar-studios-standard-library\test)";
+    }
+};
+
+#endif
