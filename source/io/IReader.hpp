26015c7b959b9a67526b43631b1a9e1b5a04a3cf
Patrick <webmaster@lynarstudios.com>
2020-08-17 16:28:23 +0200

    Added IReader interface
    - added IReader interface to provide basic
    functionality for reading

---
+++ source/io/IReader.hpp
@@ -0,0 +1,26 @@
+/*
+ * Author:          Patrick-Christopher Mattulat
+ * Company:         Lynar Studios
+ * E-Mail:          webmaster@lynarstudios.com
+ * Created:         2020-08-17
+ * Changed:         2020-08-17
+ *
+ * */
+
+#ifndef I_READER_HPP
+#define I_READER_HPP
+
+#include "../base/Types.hpp"
+
+namespace ls_std {
+  class IReader {
+    public:
+
+      IReader() = default;
+      ~IReader() = default;
+
+      virtual ls_std::byte* read() = 0;
+  };
+}
+
+#endif
