b9fb195f731abe4b033cd0176ecd8c02d180e814
pcmattulat <pcmattulat@primervoto.de>
2020-08-30 10:09:49 +0200

    Added SerializableJSONString class
    - added SerializableJSONString class to offer possibility to serialize
    ls_std::String class
    - added tests for SerializableJSONString class

---
+++ source/serialization/boxing/SerializableJSONString.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author:          Patrick-Christopher Mattulat
+ * Company:         Lynar Studios
+ * E-Mail:          webmaster@lynarstudios.com
+ * Created:         2020-08-30
+ * Changed:         2020-08-30
+ *
+ * */
+
+#include "SerializableJSONString.hpp"
+
+ls_std::SerializableJSONString::SerializableJSONString(std::shared_ptr<ls_std::String> _value) : Class("SerializableJSONString"),
+value(std::move(_value))
+{}
+
+ls_std::byte_field ls_std::SerializableJSONString::marshal()
+{
+  this->_update();
+  return this->jsonObject.dump();
+}
+
+void ls_std::SerializableJSONString::unmarshal(const ls_std::byte_field& _data)
+{
+  std::string jsonString = std::string(_data);
+  this->jsonObject = nlohmann::json::parse(jsonString);
+
+  if(this->jsonObject.contains("value")) {
+    *this->value = this->jsonObject["value"];
+  }
+}
+
+void ls_std::SerializableJSONString::_update()
+{
+  this->jsonObject = {
+      {"class", this->value->getClassName()},
+      {"value", this->value->toString()}
+  };
+}
