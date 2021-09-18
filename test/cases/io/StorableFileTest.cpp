47c9c4af92b17af1d0ead19e224a06098429ab18
Patrick-Christopher Mattulat <webmaster@lynarstudios.com>
2021-09-18 09:21:47 +0200

    Reformat StorableFileTest

--- test/cases/io/StorableFileTest.cpp
+++ test/cases/io/StorableFileTest.cpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2020-08-19
- * Changed:         2021-04-23
+ * Changed:         2021-09-18
  *
  * */
 
