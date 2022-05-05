9f96cf67358de038cfc3e4c3fc5399b71e31e1cf
public <7iW2mE8m?!>
2022-05-05 12:40:57 +0200

    Reorder source files and test files in cmake

--- source/ls_std/event/serialization/SerializableJsonEvent.cpp
+++ source/ls_std/event/serialization/SerializableJsonEvent.cpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2020-12-07
- * Changed:         2021-09-19
+ * Changed:         2022-05-05
  *
  * */
 
--- source/ls_std/logic/serialization/SerializableJsonStateConnection.cpp
+++ source/ls_std/logic/serialization/SerializableJsonStateConnection.cpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2020-09-14
- * Changed:         2021-05-02
+ * Changed:         2022-05-05
  *
  * */
 
