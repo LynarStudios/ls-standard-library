9669da65b15fe869663bf3cee1bae60bccd067d4
Patrick-Christopher Mattulat <webmaster@lynarstudios.com>
2022-06-26 22:56:27 +0200

    Add msvc dll definition header

---
+++ include/ls_std/msvc_dll_definitions.hpp
@@ -0,0 +1,15 @@
+/*
+ * Author:          Patrick-Christopher Mattulat
+ * Company:         Lynar Studios
+ * E-Mail:          webmaster@lynarstudios.com
+ * Created:         2022-06-26
+ * Changed:         2022-06-26
+ *
+ * */
+
+#ifndef LS_STD_MSVC_DLL_DEFINITIONS_HPP
+#define LS_STD_MSVC_DLL_DEFINITIONS_HPP
+
+#define DLL_EXPORT        __declspec(dllexport)
+
+#endif
