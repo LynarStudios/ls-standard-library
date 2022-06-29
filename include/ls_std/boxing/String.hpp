baacbfd6e3f2bcb900b44f4adf445318146e37c0
Patrick-Christopher Mattulat <webmaster@lynarstudios.com>
2022-06-29 22:12:15 +0200

    Add dynamic goal to "boxing" module

--- include/ls_std/boxing/String.hpp
+++ include/ls_std/boxing/String.hpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2020-08-14
- * Changed:         2022-05-19
+ * Changed:         2022-06-29
  *
  * */
 
@@ -16,6 +16,7 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <ls_std/os/dynamic_goal.hpp>
 
 namespace ls
 {
@@ -23,7 +24,7 @@ namespace ls
   {
     namespace boxing
     {
-      class String : public ls::std::core::Class, public ls::std::core::interface_type::IBoxing
+      class DYNAMIC_GOAL String : public ls::std::core::Class, public ls::std::core::interface_type::IBoxing
       {
         public:
 
