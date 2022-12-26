6ddba96da42f1c83efb2451f88f0af751bbe543e
Patrick-Christopher Mattulat <webmaster@lynarstudios.com>
2022-12-26 11:53:07 +0100

    Remove TODO claiming to add dynamic goal
    This doesn't make sense with POSIX classes.

--- include/ls_std/common/interface/IPosixReader.hpp
+++ include/ls_std/common/interface/IPosixReader.hpp
@@ -3,7 +3,7 @@
  * Company:         Lynar Studios
  * E-Mail:          webmaster@lynarstudios.com
  * Created:         2022-12-15
- * Changed:         2022-12-25
+ * Changed:         2022-12-26
  *
  * */
 
@@ -14,7 +14,7 @@
 
 namespace ls::std::common::interface_type
 {
-  class IPosixReader // TODO: add dynamic goal
+  class IPosixReader
   {
     public:
 
