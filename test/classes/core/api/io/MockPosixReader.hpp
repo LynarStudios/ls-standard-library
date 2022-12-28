e56a1cfd9d5748c08ce5065d396f47321360c4b4
Patrick-Christopher Mattulat <webmaster@lynarstudios.com>
2022-12-28 10:18:47 +0100

    Remove TODO's requesting module move of Socket mock classes
    This was already done.

--- test/classes/core/api/io/MockPosixReader.hpp
+++ test/classes/core/api/io/MockPosixReader.hpp
@@ -15,7 +15,7 @@
 
 namespace ls_std_core_test
 {
-  class MockPosixReader : public ls::std::common::interface_type::IPosixReader // TODO: move to "common" module
+  class MockPosixReader : public ls::std::common::interface_type::IPosixReader
   {
     public:
 
